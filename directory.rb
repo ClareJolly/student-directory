38e352ff6bd207eb694abd49e61203809ab977a8
Clare Jolly <clarejollyuk@gmail.com>
2018-09-08 23:38:30 +0100

    allow user input to add students

--- directory.rb
+++ directory.rb
@@ -24,7 +24,26 @@ end
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    #add the student has to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of input_students
+  students
+end
 # nothing happens until we call the methods.
+students = input_students
 print_header
 print(students)
 print_footer(students)
