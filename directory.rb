78d5564a0cd731f113f2e1af1b0b7456162dd0fd
Clare Jolly <clarejollyuk@gmail.com>
2018-09-10 20:57:18 +0100

    interactive menu added and working

--- directory.rb
+++ directory.rb
@@ -94,18 +94,44 @@ def input_students
 end
 
 def interactive_menu
-  # 1. print the menu and ask the user what to do
-  # 2. read the input and save it into a variable
-  # 3. do what the user has asked
-  # 4. repeat from step 1
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" # 9 beacause we'll be adding more items
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+    when "1"
+      # input the students
+      students = input_students
+    when "2"
+      # show the students
+      if students.count > 0
+        print_header
+        print(students)
+        print_footer(students)
+      else
+        puts "No Students :-("
+      end
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
 end
 
 # nothing happens until we call the methods.
-students = input_students
-if students.count > 0
-  print_header
-  print(students)
-  print_footer(students)
-else
-  puts "No Students :-("
-end
+interactive_menu
+
+# students = input_students
+# if students.count > 0
+  # print_header
+  # print(students)
+  # print_footer(students)
+# else
+  # puts "No Students :-("
+# end
