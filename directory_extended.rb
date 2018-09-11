0dc562afe0104abdf3d5d75d95749ce13f86f8cd
Clare Jolly <clarejollyuk@gmail.com>
2018-09-11 21:54:26 +0100

    more exercise - open file and auto close

--- directory_extended.rb
+++ directory_extended.rb
@@ -66,23 +66,25 @@ def save_students
     puts "Enter the filename you want to use"
     filename = STDIN.gets.chomp
     # open the file for writing
-    file = File.open(filename, "w")
-    # iterate over the array of students
-    @students.each do |student|
-      student_data = [student[:name],student[:cohort]]
-      csv_line = student_data.join(",")
-      file.puts csv_line
+    file = File.open(filename, "w") do |file|
+      # iterate over the array of students
+      @students.each do |student|
+        student_data = [student[:name],student[:cohort]]
+        csv_line = student_data.join(",")
+        file.puts csv_line
+      end
     end
-    file.close
+    # file.close
 end
 
 def load_students(filename = "students.csv")
-  file = File.open(filename, "r")
-  file.readlines.each do |line|
-    name, cohort = line.chomp.split(",")
-    write_to_array(name, cohort)
+  file = File.open(filename, "r") do |file|
+    file.readlines.each do |line|
+      name, cohort = line.chomp.split(",")
+      write_to_array(name, cohort)
+    end
   end
-  file.close
+  # file.close
 end
 
 def try_load_students
