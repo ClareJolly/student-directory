5fea25ecddae3885b6ebfb0590fe38998718d6f3
Clare Jolly <clarejollyuk@gmail.com>
2018-09-12 20:49:09 +0100

    more exercise - rearranged methods

--- directory_extended.rb
+++ directory_extended.rb
@@ -9,21 +9,6 @@
 require "csv.rb"
 @students = [] # an empty array accessible to all methods
 
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  # get the first name
-  name = STDIN.gets.chomp
-  # while the name is not empty, repeat this code
-  while !name.empty? do
-    # add the student hash to the array
-    write_to_array(name, cohort="november")
-    puts "Now we have #{@students.count} students"
-    # get another name from the user
-    name = STDIN.gets.chomp
-  end
-end
-
 def interactive_menu
   loop do
     print_menu
@@ -39,12 +24,6 @@ def print_menu
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
-def show_students
-  print_header
-  print_student_list
-  print_footer
-end
-
 def process_menu_selection(selection)
   case selection
     when "1" then input_students
@@ -53,6 +32,21 @@ def process_menu_selection(selection)
     when "4" then load_students
     when "9" then exit # this will cause the program to terminate
     else puts "I don't know what you meant, try again"
+  end
+end
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # get the first name
+  name = STDIN.gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    write_to_array(name, cohort="november")
+    puts "Now we have #{@students.count} students"
+    # get another name from the user
+    name = STDIN.gets.chomp
   end
 end
 
@@ -69,6 +63,12 @@ end
 
 def print_footer
   puts "Overall, we have #{@students.count} great students"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
 end
 
 def save_students
